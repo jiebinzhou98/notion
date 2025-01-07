239f640dd0cf9110a98e06845fbddb2839d445d0
jiebinzhou98 <benzhou98@outlook.com>
2025-01-07 15:57:09 -0500

    style: :zap: implement typewriter effect to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 import { Button } from "@/components/ui/button";
+import TypewriterTitle from "@/components/ui/TypewriterTitle";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button>Click me</Button>
+    <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-semibold text-7xl text-center">
+          AI <span className="text-green-600 font-bold">note taking</span>{" "}
+          assistance.
+        </h1>
+        <div className="mt-4"></div>
+        <h2 className="font-semibold text-3xl text-center">
+          <TypewriterTitle/>
+        </h2>
+      </div>
+
+    </div>
+
+    
   );
 }
