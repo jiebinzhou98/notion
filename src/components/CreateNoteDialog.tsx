455d6e7a39dddbd989691da587dbaf4d3b991dcc
jiebinzhou98 <benzhou98@outlook.com>
2025-01-12 14:43:53 -0500

    perf: :art: implement create and cancel feature for the dialog

--- src/components/CreateNoteDialog.tsx
+++ src/components/CreateNoteDialog.tsx
@@ -2,6 +2,9 @@
 import { Dialog, DialogTrigger } from '@radix-ui/react-dialog'
 import { Plus } from 'lucide-react'
 import React from 'react'
+import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from './ui/dialog'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
 
 type Props = {}
 
@@ -9,13 +12,33 @@ const CreateNoteDialog = (props: Props) => {
   return (
     <Dialog>
         <DialogTrigger>
-            <div className='border-dashed border-2 border-green-600 h-full rounded-lg items-center justify-center sm:flex-col hover:shadow-xl transition hover:-translate-y-1 flex-row p-4'>
+            <div className='border-dashed border-2 flex border-green-600 h-full rounded-lg items-center justify-center sm:flex-col hover:shadow-xl transition hover:-translate-y-1 flex-row p-4'>
                 <Plus className='w-6 h-6 text-green-600' strokeWidth={3}/>
                 <h2 className='font-semibold text-green-600 sm-mt-2'>New Note Book</h2>
             </div>
         </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>New Note Book</DialogTitle>
+                <DialogDescription>
+                You can create a new note by clicking the button below.
+                </DialogDescription>
+            </DialogHeader>
+            <form>
+              <Input placeholder='Name...'/>
+              <div className='h-4'></div>
+              <div className='flex items-center gap-2'>
+                <Button type='reset' variant={'secondary'}>
+                  Cancel
+                  </Button>
+                <Button type='submit' className='bg-green-600'>
+                  Create
+                  </Button>
+              </div>
+            </form>
+        </DialogContent>
     </Dialog>
   )
 }
 
-export default CreateNoteDialog
+export default CreateNoteDialog;
